c37d878aabf82846a4c6537ddb536d0a1d34d474
DPubliweb <dan@publiweb.agency>
2025-12-24 12:52:16 +0200

    import corrector

--- src/components/landing/EligibilityForm.jsx
+++ src/components/landing/EligibilityForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardContent, CardHeader } from "components/ui/card";
+import { Card, CardContent, CardHeader } from "../ui/card";
 import { CheckCircle2, Loader2 } from "lucide-react";
 
 // ---- LOG LOCAL + QODDI ----
